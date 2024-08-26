ad0f7644e0609669deb2cb5150befbb46b124505
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-08-26 21:56:27 +0530

    Initial Commit

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <div className='landing-page-navbar'>
+        
+    </div>
+  )
+}
+
+export default NavBar;
---
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LandingPage;
